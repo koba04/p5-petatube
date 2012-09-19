063fdf45b51a3ae6be3a9727a8b71a57de03e3da
Toru, Kobayashi <koba0004@gmail.com>
2012-09-20 01:32:04 +0900

    support ios (non flash)

--- static/js/view/video.js
+++ static/js/view/video.js
@@ -2,10 +2,26 @@
 
 PetaTube.View.Video = Backbone.View.extend({
   player: null,
-  el: "#video",
+  el: "#play-video",
   initialize: function(args) {
     this.videos = args.videos;
     this.videos.on("reset", this.resetPlay, this);
+  },
+  events: {
+    "click #prev-button": "prev",
+    "click #next-button": "next",
+  },
+  next: function() {
+    var nextId = this.videos.next();
+    if ( nextId ) {
+      this.play(nextId);
+    }
+  },
+  prev: function() {
+    var prevId = this.videos.prev();
+    if ( prevId ) {
+      this.play(prevId);
+    }
   },
   resetPlay: function(videos) {
     var video = videos.at(this.videos.currentIndex);
@@ -22,28 +38,39 @@ PetaTube.View.Video = Backbone.View.extend({
     } else {
       this.initSwf(videoId);
     }
+    var tmpl = $('#tmpl-button').html();
+    var $panel = _.template(tmpl, { current: this.videos.currentIndex + 1, total: this.videos.length });
+    this.$el.find("#video-panel").html($panel);
+
   },
   initSwf: function(videoId) {
     var self = this;
-    swfobject.embedSWF(
-      "http://www.youtube.com/v/" + videoId + "?enablejsapi=1",
-      "video",
-      "800",
-      "450",
-      "8",
-      null,
-      null,
-      { allowScriptAccess: "always" },
-      { id: "player" }
-    );
-    window.onYouTubePlayerReady = function() {
-      self.player = document.getElementById("player");
-      self.player.addEventListener("onStateChange", "onplayerStateChange");
-    };
-    window.onplayerStateChange = function(newState) {
-      if (newState === 0 ) {
-        self.play(self.videos.next());
-      }
+    // https://developers.google.com/youtube/iframe_api_reference
+    var tag = document.createElement('script');
+    tag.src = "//www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    window.onYouTubeIframeAPIReady = function() {
+      self.player = new YT.Player('video', {
+        height: '450',
+        width: '800',
+        videoId: videoId,
+        events: {
+          'onReady': function(e) {
+            // doesn't play video on iphone.
+            // e.target.playVideo()
+          },
+          'onStateChange': function(e) {
+            var state = e.data;
+            if (state == YT.PlayerState.ENDED) {
+              self.play(self.videos.next());
+            }
+          },
+          'onError': function(e) {
+            self.play(self.videos.next());
+          }
+        }
+      });
     };
   }
 });
