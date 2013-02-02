8568c81336bdcac470bdb85de408fc6e04ac0548
Toru, Kobayashi <koba0004@gmail.com>
2013-02-02 18:04:07 +0900

    compass compile by grunt.js

---
+++ compass_config.rb
@@ -0,0 +1,7 @@
+http_path       = "/"
+sass_dir        = "src/scss"
+css_dir         = "static/css"
+images_dir      = "static/img"
+javascripts_dir = "static/js"
+output_style    = (environment == :production) ? :compressed : :nested
+line_comments   = (environment == :production) ? false : true
