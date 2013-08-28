9a80bd80315d99a475bd63fc1837c9fdc52d4c93
Toru, Kobayashi <koba0004@gmail.com>
2013-08-28 09:55:16 +0900

    check supervisord is running

--- server/development/vagrant/spec/petatube/supervisor_spec.rb
+++ server/development/vagrant/spec/petatube/supervisor_spec.rb
@@ -4,6 +4,10 @@ describe package('supervisor') do
   it { should be_installed }
 end
 
+describe service('supervisord') do
+  it { should be_running }
+end
+
 describe file('/etc/supervisor/supervisord.conf') do
   it { should be_file }
 end
