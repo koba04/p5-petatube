cf4ca2a03d7893ab6054257d7126bcb752b512fe
Toru, Kobayashi <koba0004@gmail.com>
2013-08-28 09:28:04 +0900

    add nginx cookbook

--- server/development/vagrant/spec/petatube/nginx_spec.rb
+++ server/development/vagrant/spec/petatube/nginx_spec.rb
@@ -4,10 +4,6 @@ describe package('nginx') do
   it { should be_installed }
 end
 
-describe port(80) do
-  it { should be_listening }
-end
-
 describe file('/etc/nginx/nginx.conf') do
   it { should be_file }
 end
