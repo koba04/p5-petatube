df0181664b19c86fb9f776e43c7006f1a29a77a9
Toru, Kobayashi <koba0004@gmail.com>
2013-08-30 10:05:39 +0900

    *env script to absolute path. (spec)

--- server/development/vagrant/spec/petatube/anyenv_spec.rb
+++ server/development/vagrant/spec/petatube/anyenv_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe command('perl -v') do
+describe command('/home/vagrant/.anyenv/envs/plenv/shims/perl -v') do
   it { should return_stdout /v5\.18\.1/ }
 end
 
-describe command('ruby -v') do
+describe command('/home/vagrant/.anyenv/envs/rbenv/shims/ruby -v') do
   it { should return_stdout /ruby\s2\.0\.0p247/ }
 end
 
-describe command('node -v') do
+describe command('/home/vagrant/.anyenv/envs/ndenv/shims/node -v') do
   it { should return_stdout /v0\.10\.17/ }
 end
 
