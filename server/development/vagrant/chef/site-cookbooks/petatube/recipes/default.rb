a8dbe6c30a7ab080e5802698d3425cb1ae5f33a0
Toru, Kobayashi <koba0004@gmail.com>
2013-09-02 09:29:36 +0900

    use renewal brunch

--- server/development/vagrant/chef/site-cookbooks/petatube/recipes/default.rb
+++ server/development/vagrant/chef/site-cookbooks/petatube/recipes/default.rb
@@ -13,6 +13,7 @@ bash "git clone" do
   environment "HOME" => node[:user][:home]
   code <<-EOC
     git clone https://github.com/koba04/p5-petatube.git petatube
+    git checkout -b renewal origin/renewal
     cd petatube
     git submodule init
     git submodule update
