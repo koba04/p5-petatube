ffdd9a71777ce5414fb6397e6f10249d95dbaae4
Toru, Kobayashi <koba0004@gmail.com>
2013-09-13 01:17:16 +0900

    start_server exec by carton

--- server/development/supervisor/petatube.sh
+++ server/development/supervisor/petatube.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 exec 2>&1
-exec /home/vagrant/.anyenv/envs/plenv/versions/5.18.1/bin/start_server --port=9001 -- \
-/home/vagrant/.anyenv/envs/plenv/versions/5.18.1/bin/plackup -s Starlet -E development \
+exec /home/vagrant/.anyenv/envs/plenv/versions/5.18.1/bin/carton exec -- start_server --port=9001 \
+-- plackup -s Starlet -E development \
 --max-workers=3 \
 --max-keepalive-reqs=1 \
 --max-reqs-per-child=10000 \
--a /home/vagrant/petatube/app.psgi
+/home/vagrant/petatube/app.psgi
