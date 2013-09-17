fc6411c0c57247b0ea132e6e8ce113aa593311e3
Toru, Kobayashi <koba0004@gmail.com>
2013-09-17 10:00:16 +0900

    remove max-reqs-per-child and max-keepalive-reqs option

--- server/production/supervisor/petatube.sh
+++ server/production/supervisor/petatube.sh
@@ -4,6 +4,4 @@ exec 2>&1
 exec /home/koba04/perl5/perlbrew/perls/perl-5.18.1/bin/carton exec -- start_server --port=9001 \
 -- plackup -s Starlet -E production \
 --max-workers=3 \
---max-keepalive-reqs=1 \
---max-reqs-per-child=10000 \
 /home/koba04/petatube/app/app.psgi
